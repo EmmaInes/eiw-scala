c89bcb4b48ddb2e21ed98283fabe8f68bf27f418
EmmaInes <emma.wahnish@groovinads.com>
2024-03-07 18:45:24 -0300

    demo

---
+++ demo.scala
@@ -0,0 +1,22 @@
+object demo {
+  def main(args: Array[String]) : Unit ={
+    var myInt : Int = 5
+    var myString : String = "Hello, World!"
+    var myDouble : Double = 5.5
+    myDouble = 6.5
+    var myBool : Boolean = true
+    var myChar : Char = 'a'
+    var myFloat : Float = 5.5f
+    var myShort : Short = 5
+    var myLong : Long = 5
+    var myByte : Byte = 5
+    println(myInt)
+    println(myString)
+    println(myDouble)
+    println(myBool)
+    println(myChar)
+    println(myFloat)
+    println(myShort)
+    println(myLong)
+  }
+}
